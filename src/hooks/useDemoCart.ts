a86ef5f4249cf438b9690533559868d740a9d51d
emersongabriel29-pixel <emersongabriel29@gmail.com>
2026-09-24 23:53:58 -0300

    style: align cart hook with prettier layout

--- src/hooks/useDemoCart.ts
+++ src/hooks/useDemoCart.ts
@@ -6,10 +6,7 @@ import { usePersistentState } from "../usePersistentState";
 export function useDemoCart(notify: (message: string) => void) {
   const [cart, setCart] = usePersistentState<Record<number, number>>("feirae:cart", {});
 
-  const cartProducts = useMemo(
-    () => products.filter((product) => cart[product.id]),
-    [cart],
-  );
+  const cartProducts = useMemo(() => products.filter((product) => cart[product.id]), [cart]);
   const subtotal = useMemo(() => cartSubtotal(cartProducts, cart), [cartProducts, cart]);
   const itemCount = useMemo(
     () => Object.values(cart).reduce((sum, quantity) => sum + quantity, 0),
